73bdfd6bfa95490ee104598ac73185deb6193041
EsinShadrach <esinshadrachrafe@gmail.com>
2023-09-13 14:42:29 +0100

    error fix

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center w-full min-h-screen text-center">
+      Page Not Found - API only
+    </div>
+  );
+}
