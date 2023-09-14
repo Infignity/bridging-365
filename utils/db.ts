7a79ec063ff8f280bbc6a57c9e4d2268bbd88936
EsinShadrach <esinshadrachrafe@gmail.com>
2023-09-14 17:31:42 +0100

    changed db

--- utils/db.ts
+++ utils/db.ts
@@ -10,7 +10,7 @@ export const connectToDB = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: "bridging-365",
+      dbName: "Cluster0",
     });
 
     isConnected = true;
